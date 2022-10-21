c86b882e615ed1a3bcb70681bc39dfe3fc7441dc
«Zukhra» <sssmailll@mail.ru>
2022-10-21 10:48:54 +0300

    module write

--- write.py
+++ write.py
@@ -1,4 +1,28 @@
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#   author Zukhra
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+import Edit
+import add_delete
+import csv
+import read
+import logger
+
+# запись справочника
+def write_employees_new():
+    emp = add_delete.new_employee()
+    employees = read.read_csv()
+    employees = employees + emp
+    with open('names.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(employees)
 
 
-def write_file():
-    pass
+
+def write_employees():
+    employees = Edit.edit_employees()
+    txt_msg = 'перезаписан список сотрудников '
+    logger.number_logger(txt_msg)
+    with open('names.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(employees)
